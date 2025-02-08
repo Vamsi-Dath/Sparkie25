eab349219adb6ad43b0742fb604bb880e0e8444c
Rushikesh Joshi <rushikeshjoshicv@gmail.com>
2025-02-08 05:51:00 -0600

    testing done with get_weather

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -8,15 +8,9 @@ def get_weather(request):
     longitude = request.GET.get("lon")
 
 
-    #complete testing if needed
-    if not latitude or not longitude:
-        return JsonResponse({"error": "Latitude and longitude are required"}, status=400)
-
     url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation&temperature_unit=fahrenheit&wind_speed_unit=mph"
 
-    try:
-        response = requests.get(url)
-        data = response.json()
-        return JsonResponse(data)
-    except Exception as e:
-        return JsonResponse({"error": "Error fetching weather data", "details": str(e)}, status=500)
+    response = requests.get(url)
+    data = response.json()
+    return JsonResponse(data)
+
