9e9a10c50ba5c239f359141493168dc0388d941b
Rushikesh Joshi <rushikeshjoshicv@gmail.com>
2025-02-08 03:58:56 -0600

    weather api function 1

--- backend/views.py
+++ backend/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import requests
+from django.http import JsonResponse
 
-# Create your views here.
+def get_weather(request):
+    latitude = request.GET.get("lat")
+    longitude = request.GET.get("lon")
+
+
+
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation&temperature_unit=fahrenheit&wind_speed_unit=mph"
+
+    response = requests.get(url)
+    data = response.json()
+    return JsonResponse(data)
+
