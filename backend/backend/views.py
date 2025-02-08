904aabebb1a6526eb979d0f6b3431c173d38d12a
Rushikesh Joshi <rushikeshjoshicv@gmail.com>
2025-02-08 06:59:45 -0600

    basic working api frontend/backend integration

--- backend/backend/views.py
+++ backend/backend/views.py
@@ -8,7 +8,7 @@ def get_weather(request):
     longitude = request.GET.get("lon")
 
 
-    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=temperature_2m,precipitation&temperature_unit=fahrenheit&wind_speed_unit=mph"
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current=temperature_2m,relative_humidity_2m,precipitation,cloud_cover,wind_speed_10m&temperature_unit=fahrenheit&wind_speed_unit=mph&timezone=America%2FChicago"
 
     response = requests.get(url)
     data = response.json()
